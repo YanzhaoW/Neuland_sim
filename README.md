8b7d64ffc8359191997370ee5328a54a9431e400
YanzhaoW <yanzhao960808@gmail.com>
2022-06-20 18:23:01 +0200

    add readme and sim1.C

---
+++ README.md
@@ -0,0 +1,41 @@
+## How to download this repository
+Run following command in your terminal:
+```text
+git clone https://github.com/YanzhaoW/Neuland_sim.git
+```
+
+
+## How to run R3BRoot macros
+NOTE: To continue the following steps, access to GSI servers is required.
+
+### step 1:
+Log in to a GSI analysis server (`lxlandana01` or `lxlandana02`) with your own account and run the following command:
+```text
+source /u/land/fake_cvmfs/sourceme.sh
+```
+The command above cannot work in other GSI servers, such as the server you log in to just using `ssh username@lx-pool.gsi.de`.
+
+### step 2:
+Choose any folder and inside it download R3BRoot with
+```text
+git clone https://github.com/R3BRootGroup/R3BRoot.git
+cd R3BRoot
+git checkout dev
+git clone https://github.com/R3BRootGroup/macros.git
+cd macros
+```
+
+### step 3:
+Under the root folder `R3BRoot/`, compile the whole project with:
+```text
+mkdir build
+cd build
+cmake ..
+make -j8
+```
+where the number `8` after `-j` in the last line represents the number of cores that are used for the compilation. You can choose any number of cores you would like.
+
+If the compilation is successful, run the following command under the root folder `R3BRoot/`
+```text
+source ./build/config.sh
+```
---
+++ macros/sim1.C
@@ -0,0 +1,70 @@
+int sim1(){
+
+    // ----------------------------initial parameters--------------------------------------------
+    const TString workDirectory = getenv("VMCWORKDIR");
+    const TString simufile = "test.simu.root";
+    const TString parafile = "test.para.root";
+    const Int_t eventNum = 100;
+    const Int_t printEventNum = 100;
+
+    FairBoxGenerator* boxGen = new FairBoxGenerator{2112, 4};
+    boxGen->SetXYZ(0, 0, 0.);
+    boxGen->SetThetaRange(0., 3.);
+    boxGen->SetPhiRange(0., 360.);
+    boxGen->SetEkinRange(0.6, 0.6);
+
+    neuland = new R3BNeuland(30, {0., 0., 1650.});
+
+    // ----------------------------Start timer--------------------------------------------
+    TStopwatch* timer = new TStopwatch();
+    timer->Start();
+    FairLogger::GetLogger()->SetLogVerbosityLevel("verylow");
+    FairLogger::GetLogger()->SetLogScreenLevel("nolog");
+    // FairLogger::GetLogger()->SetLogToScreen(false);
+    gSystem->Setenv("GEOMPATH", workDirectory + "/geometry");
+    gSystem->Setenv("CONFIG_DIR", workDirectory + "/gconfig");
+
+    // ----------------------------define simulation run--------------------------------------------
+    FairRunSim* run = FairRunSim::Instance();
+    run->SetName("TGeant4");
+    run->SetStoreTraj(false);
+    run->SetMaterials("media_r3b.geo");
+    run->SetSink(new FairRootFileSink(simufile));
+
+    // ----------------------------create particle generation--------------------------------------------
+    FairPrimaryGenerator* primGen = new FairPrimaryGenerator;
+    primGen->AddGenerator(boxGen);
+    run->SetGenerator(primGen);
+
+    // ----------------------------specify geometry--------------------------------------------
+    // Geometry: Cave
+    R3BCave* cave = new R3BCave("CAVE");
+    cave->SetGeometryFileName("r3b_cave.geo");
+    run->AddModule(cave);
+    run->AddModule(neuland);
+
+    // ----------------------------Initialize run--------------------------------------------
+    run->Init();
+    auto grun = G4RunManager::GetRunManager();
+    grun->SetPrintProgress(printEventNum);
+    auto event = dynamic_cast<TG4EventAction*>(
+        const_cast<G4UserEventAction*>(grun->GetUserEventAction()));
+    event->VerboseLevel(0);
+
+    // ----------------------------Specify parameter output--------------------------------------------
+    FairParRootFileIo* parFileIO = new FairParRootFileIo(true);
+    parFileIO->open(parafile);
+    auto rtdb = run->GetRuntimeDb();
+    rtdb->setOutput(parFileIO);
+    rtdb->saveOutput();
+
+    // ----------------------------start run--------------------------------------------
+    run->Run(eventNum);
+
+    // ----------------------------stop the timer--------------------------------------------
+    timer->Stop();
+    std::cout << "CPU time: " << timer->CpuTime() << std::endl;
+
+
+    return 0;
+}
