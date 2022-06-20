a1cc96b406d1bd532bd16435f4b89270dc793f8a
YanzhaoW <yanzhao960808@gmail.com>
2022-06-20 19:13:13 +0200

    update sim1.C

--- macros/sim1.C
+++ macros/sim1.C
@@ -4,7 +4,7 @@ int sim1(){
     const TString workDirectory = getenv("VMCWORKDIR");
     const TString simufile = "test.simu.root";
     const TString parafile = "test.para.root";
-    const Int_t eventNum = 100;
+    const Int_t eventNum = 10000;
     const Int_t printEventNum = 100;
 
     FairBoxGenerator* boxGen = new FairBoxGenerator{2112, 4};
@@ -13,7 +13,7 @@ int sim1(){
     boxGen->SetPhiRange(0., 360.);
     boxGen->SetEkinRange(0.6, 0.6);
 
-    neuland = new R3BNeuland(30, {0., 0., 1650.});
+    auto neuland = new R3BNeuland(30, {0., 0., 1650.});
 
     // ----------------------------Start timer--------------------------------------------
     TStopwatch* timer = new TStopwatch();
@@ -25,7 +25,7 @@ int sim1(){
     gSystem->Setenv("CONFIG_DIR", workDirectory + "/gconfig");
 
     // ----------------------------define simulation run--------------------------------------------
-    FairRunSim* run = FairRunSim::Instance();
+    FairRunSim* run = new FairRunSim();
     run->SetName("TGeant4");
     run->SetStoreTraj(false);
     run->SetMaterials("media_r3b.geo");
@@ -45,11 +45,11 @@ int sim1(){
 
     // ----------------------------Initialize run--------------------------------------------
     run->Init();
-    auto grun = G4RunManager::GetRunManager();
-    grun->SetPrintProgress(printEventNum);
-    auto event = dynamic_cast<TG4EventAction*>(
-        const_cast<G4UserEventAction*>(grun->GetUserEventAction()));
-    event->VerboseLevel(0);
+    // auto grun = G4RunManager::GetRunManager();
+    // grun->SetPrintProgress(printEventNum);
+    // auto event = dynamic_cast<TG4EventAction*>(
+    //     const_cast<G4UserEventAction*>(grun->GetUserEventAction()));
+    // event->VerboseLevel(0);
 
     // ----------------------------Specify parameter output--------------------------------------------
     FairParRootFileIo* parFileIO = new FairParRootFileIo(true);
